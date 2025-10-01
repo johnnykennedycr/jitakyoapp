4a6b06cfde6e5e323fa7996c3078c4c5c4f206ac
johnnykennedycr <jkatitude@gmail.com>
2025-10-01 20:28:38 -0300

    Teste cors

--- frontend/aluno/main.js
+++ frontend/aluno/main.js
@@ -1,4 +1,4 @@
-import { auth, db } from './firebase.js';
+import { auth } from './firebase.js';
 import { onAuthStateChanged, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 // --- CONFIGURAÇÃO ---
